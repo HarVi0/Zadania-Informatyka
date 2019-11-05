1f9c2ff7001f1481a95ea1593cd070de4acaaeb7
HarVi0 <55979894+HarVi0@users.noreply.github.com>
2019-11-05 20:57:07 +0100

    Create Zad 8.cpp

---
+++ Zad 8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float suma, i;
+
+
+        while(i!=0){
+            cout<<"Podaj liczbe: ";
+            cin>>i;
+
+            suma= suma + i;
+
+            cout<<"Suma podanych liczb: "<<suma<<endl;
+        }
+
+
+    return 0;
+}
