54fc2b6c3c34e06abb69d829fd8ac8a89215622d
HarVi0 <55979894+HarVi0@users.noreply.github.com>
2019-11-09 23:20:02 +0100

    Create Zad 10.cpp

---
+++ Zad 10.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+
+void euro (float x)
+{
+   cout<<"Twoja kwota w Euro: "<<x*4,2638;
+}
+
+void dolar (float y)
+{
+   cout<<"Twoja kwota w Dolarach Amerykanskich: "<<y*3,8625;
+}
+
+
+int main()
+{
+    float zl;
+
+    cout<<"Podaj kwote w zlotowkach: ";
+    cin>>zl;
+    cout<<endl;
+    cout<<"Kurs Dolara Amerykanskiego na dzien 9.11.2019: 3,8625"<<endl;
+    cout<<"Kurs Euro na dzien 9.11.2019: 4,2638 "<<endl;
+    cout<<endl;
+    euro(zl);
+    cout<<endl;
+    dolar(zl);
+
+
+    return 0;
+}
