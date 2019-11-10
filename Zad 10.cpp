d552579169347392d113fd0984e67ecaa9868c7d
HarVi0 <55979894+HarVi0@users.noreply.github.com>
2019-11-10 19:03:30 +0100

    Update Zad 10.cpp

--- Zad 10.cpp
+++ Zad 10.cpp
@@ -5,29 +5,69 @@ using namespace std;
 
 void euro (float x)
 {
-   cout<<"Twoja kwota w Euro: "<<x/4,2638;
+   cout<<"Twoja kwota po przeliczeniu na Euro: "<<x/4,2638;
 }
 
 void dolar (float y)
 {
-   cout<<"Twoja kwota w Dolarach Amerykanskich: "<<y/3,8625;
+   cout<<"Twoja kwota po przeliczeniu na Dolary Amerykanskie: "<<y/3,8625;
 }
 
 
 int main()
 {
     float zl;
+    int waluta;
+
+
 
     cout<<"Podaj kwote w zlotowkach: ";
     cin>>zl;
     cout<<endl;
-    cout<<"Kurs Dolara Amerykanskiego na dzien 9.11.2019: 3,8625"<<endl;
-    cout<<"Kurs Euro na dzien 9.11.2019: 4,2638 "<<endl;
-    cout<<endl;
-    euro(zl);
-    cout<<endl;
-    dolar(zl);
+
+    cout<<"1. Dolar"<<endl;
+    cout<<"2. Euro"<<endl;
+    cout<<"3. Oba"<<endl<<endl;
+
+    cout<<"Na jaka walute chcesz przeliczyc twoja kwote: ";
+    cin>>waluta;
 
 
-    return 0;
+    switch(waluta)
+    {
+        case 1:
+            cout<<endl;
+            cout<<"Kurs Dolara Amerykanskiego na dzien 9.11.2019: 3,8625"<<endl;
+            cout<<endl;
+            dolar(zl);
+            cout<<endl;
+        break;
+
+        case 2:
+            cout<<endl;
+            cout<<"Kurs Euro na dzien 9.11.2019: 4,2638 "<<endl;
+            cout<<endl;
+            euro(zl);
+            cout<<endl;
+        break;
+
+        case 3:
+            cout<<endl;
+            cout<<"Kurs Dolara Amerykanskiego na dzien 9.11.2019: 3,8625"<<endl;
+            cout<<"Kurs Euro na dzien 9.11.2019: 4,2638 "<<endl;
+            cout<<endl;
+            dolar(zl);
+            cout<<endl;
+            euro(zl);
+            cout<<endl;
+        break;
+
+
+        default:
+            cout<<"Nie ma takiej opcji!";
+        break;
+    }
+
+
+   return 0;
 }
